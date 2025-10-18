45c6f25ef5fda1e6e9d6e7702bdb70a2a21092e5
Akwasititus <www.ayamgatitus@gmail.com>
2025-10-18 21:08:30 +0000

    Initial commit - GraphQL CRM with Django (customers, products, orders)

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -4,3 +4,15 @@ class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
+
+
+import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ crm/schema.py
@@ -0,0 +1,30 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Customer, Product, Order
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
+
+class Mutation(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
+    bulk_create_customers = BulkCreateCustomers.Field()
+    create_product = CreateProduct.Field()
+    create_order = CreateOrder.Field()
+
+
+class Query(graphene.ObjectType):
+    customers = graphene.List(CustomerType)
+    products = graphene.List(ProductType)
+    orders = graphene.List(OrderType)
+
+    def resolve_customers(self, info):
+        return Customer.objects.all()
+
+    def resolve_products(self, info):
+        return Product.objects.all()
+
+    def resolve_orders(self, info):
+        return Order.objects.all()
+
